6d3a7452dccda7768c58c48a7880b64d4d4d29d6
gavincook <gavincook200@gmail.com>
2023-05-15 16:36:13 +0800

    add log configuration

--- app/src/main/java/cn/gavincook/configuration/WebConfiguration.java
+++ app/src/main/java/cn/gavincook/configuration/WebConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.filter.CorsFilter;
 
 /**
@@ -25,5 +26,15 @@ public class WebConfiguration {
         configSource.registerCorsConfiguration("/**", config);
 
         return new CorsFilter(configSource);
+    }
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setMaxPayloadLength(64000);
+        return loggingFilter;
     }
 }
